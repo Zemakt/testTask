c02ab28fbce6867fcf36d089be893458f286d313
Zemakt <kostya.zemchenok.00@mail.ru>
2021-12-01 18:30:55 +0300

    commit2

--- zadanie/Header.h
+++ zadanie/Header.h
@@ -1,2 +1,5 @@
 #include <iostream>
 #include <Windows.h>
+
+
+
--- zadanie/Source.cpp
+++ zadanie/Source.cpp
@@ -1,45 +1,110 @@
 #include "Header.h"
 using namespace std;
-int LFSR_Fibonacci1(unsigned short*);
-int LFSR_Fibonacci2(unsigned int*);
-int LFSR_Fibonacci3(unsigned int*);
-int LFSR_Fibonacci4(unsigned char*);
-int GetKey(unsigned short* r1, unsigned int* r2, unsigned int* r3, unsigned char* r4);
+int LFSR1(unsigned short*);
+int LFSR2(unsigned int*);
+int LFSR3(unsigned int*);
+int LFSR4(unsigned char*);
+void Encrypt(char* msg, char* msg_secret, char* shifr, unsigned short r1, unsigned int r2, unsigned int r3, unsigned char r4);
+void Decrypt(char* shifr, char* msg_secret, char* ms, char* unshifr, unsigned short reg1, unsigned int reg2, unsigned int reg3, unsigned char reg4);
+int Key(unsigned short* reg1, unsigned int* reg2, unsigned int* reg3, unsigned char* reg4);
+void msgsize(char size, char* msg);
+void secretsize(char size, char* msg_secret);
+void unshifrsize(char size, char* unshifr);
+void shifrsize(char size, char* shifr);
+void mssize(char size, char* ms);
 int main(int argc, char* argv[])
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+    int size = 255;
+	char* msg = new char[size];
 	printf("Введите строку для шифрования: \n");
-	char* msg = new char[256];
-	char* msg_secret = new char[256]{ '\0' };
-	char* shifr = new char[256]{ '\0' };
-	char* unshifr = new char[256];
-	char* ms = new char[256];
-	gets_s(msg, 256);
-	unsigned short r1 = atoi(argv[1]);
-	printf("\n%hd\n", r1);
-	unsigned int r2 = atoi(argv[2]);
-	printf("%d\n", r2);
-	unsigned int r3 = atoi(argv[3]);
-	printf("%d\n", r3);
-	unsigned char r4 = atoi(argv[4]);
-	printf("%hhd\n", r4);
+	char* msg_secret = new char[size];
+	char* shifr = new char[size];
+	char* unshifr = new char[size];
+	char* ms = new char[size];
+	cin >> msg;
+	unsigned short reg1 = atoi(argv[1]);
+	unsigned int reg2 = atoi(argv[2]);
+	unsigned int reg3 = atoi(argv[3]);
+	unsigned char reg4 = atoi(argv[4]);
+	printf("\nСообщение которое необходимо зашифровать\n");
+	cout << msg;
+	Encrypt(msg, msg_secret, shifr, reg1, reg2, reg3, reg4);
+	Decrypt(shifr, msg_secret, ms, unshifr, reg1, reg2, reg3, reg4);
 
-	printf("\nKEY = %x\n", GetKey(&r1, &r2, &r3, &r4));
-	printf("\nТекст который необходимо зашифровать\n");
-	printf("%s", msg);
-	printf("\nЗашифрованный текст\n");
-	for (unsigned int i = 0; i < strlen(msg); i++)
+	return 0;
+}
+
+
+void msgsize(char size,char *msg) 
+{
+	char newSize = size * 2;
+	char* newmsg = new char[newSize];
+
+	memcpy(newmsg, msg, size * sizeof(char));
+
+	size = newSize;
+	delete[] msg;
+	msg = newmsg;
+}
+void shifrsize(char size, char* shifr)
+{
+	char newSize = size * 2;
+	char* newshifr = new char[newSize];
+
+	memcpy(newshifr, shifr, size * sizeof(char));
+
+	size = newSize;
+	delete[] shifr;
+	shifr = newshifr;
+}
+void unshifrsize(char size, char* unshifr)
+{
+	char newSize = size * 2;
+	char* newunshifr = new char[newSize];
+
+	memcpy(newunshifr, unshifr, size * sizeof(char));
+
+	size = newSize;
+	delete[] unshifr;
+	unshifr = newunshifr;
+}
+void secretsize(char size, char* msg_secret)
+{
+	char newSize = size * 2;
+	char* newsecret = new char[newSize];
+
+	memcpy(newsecret, msg_secret, size * sizeof(char));
+
+	size = newSize;
+	delete[] msg_secret;
+	msg_secret = newsecret;
+}
+void mssize(char size, char* ms)
+{
+	char newSize = size * 2;
+	char* newms = new char[newSize];
+
+	memcpy(newms, ms, size * sizeof(char));
+
+	size = newSize;
+	delete[] ms;
+	ms = newms;
+}
+void Encrypt(char* msg, char* msg_secret, char* shifr, unsigned short reg1, unsigned int reg2, unsigned int reg3, unsigned char reg4)
+{
+	printf("\nЗашифрованное сообщение\n");
+	for (size_t i = 0; i < strlen(msg); i++)
 	{
-		// std::cout << std::bitset<sizeof(msg[i])* CHAR_BIT>(msg[i]) << "\n";
-		char mask1 = (GetKey(&r1, &r2, &r3, &r4));
-		char mask2 = (GetKey(&r1, &r2, &r3, &r4) << 1);
-		char mask3 = (GetKey(&r1, &r2, &r3, &r4) << 2);
-		char mask4 = (GetKey(&r1, &r2, &r3, &r4) << 3);
-		char mask5 = (GetKey(&r1, &r2, &r3, &r4) << 4);
-		char mask6 = (GetKey(&r1, &r2, &r3, &r4) << 5);
-		char mask7 = (GetKey(&r1, &r2, &r3, &r4) << 6);
-		char mask8 = (GetKey(&r1, &r2, &r3, &r4) << 7);
+		char mask1 = (Key(&reg1, &reg2, &reg3, &reg4));
+		char mask2 = (Key(&reg1, &reg2, &reg3, &reg4) << 1);
+		char mask3 = (Key(&reg1, &reg2, &reg3, &reg4) << 2);
+		char mask4 = (Key(&reg1, &reg2, &reg3, &reg4) << 3);
+		char mask5 = (Key(&reg1, &reg2, &reg3, &reg4) << 4);
+		char mask6 = (Key(&reg1, &reg2, &reg3, &reg4) << 5);
+		char mask7 = (Key(&reg1, &reg2, &reg3, &reg4) << 6);
+		char mask8 = (Key(&reg1, &reg2, &reg3, &reg4) << 7);
 		msg_secret[i] = ((msg[i] ^ mask1) & 0x1) +
 			((msg[i] ^ mask2) & 0x2) +
 			((msg[i] ^ mask3) & 0x4) +
@@ -50,20 +115,21 @@ int main(int argc, char* argv[])
 			((msg[i] ^ mask8) & 0x80);
 		shifr[i] = msg_secret[i] ^ msg[i];
 		cout << shifr[i];
-
-		// std::cout << std::bitset<sizeof(shifr[i])* CHAR_BIT>(shifr[i]) << "\n";
 	}
-	printf("\nРасшифрованный текст\n");
-	for (unsigned int j = 0; j < strlen(shifr); j++)
+}
+void Decrypt(char* shifr, char* msg_secret, char* ms, char* unshifr, unsigned short reg1, unsigned int reg2, unsigned int reg3, unsigned char reg4)
+{
+	printf("\nРасшифрованное сообщение\n");
+	for (size_t j = 0; j < strlen(shifr); j++)
 	{
-		char mask1 = (GetKey(&r1, &r2, &r3, &r4));
-		char mask2 = (GetKey(&r1, &r2, &r3, &r4) << 1);
-		char mask3 = (GetKey(&r1, &r2, &r3, &r4) << 2);
-		char mask4 = (GetKey(&r1, &r2, &r3, &r4) << 3);
-		char mask5 = (GetKey(&r1, &r2, &r3, &r4) << 4);
-		char mask6 = (GetKey(&r1, &r2, &r3, &r4) << 5);
-		char mask7 = (GetKey(&r1, &r2, &r3, &r4) << 6);
-		char mask8 = (GetKey(&r1, &r2, &r3, &r4) << 7);
+		char mask1 = (Key(&reg1, &reg2, &reg3, &reg4));
+		char mask2 = (Key(&reg1, &reg2, &reg3, &reg4) << 1);
+		char mask3 = (Key(&reg1, &reg2, &reg3, &reg4) << 2);
+		char mask4 = (Key(&reg1, &reg2, &reg3, &reg4) << 3);
+		char mask5 = (Key(&reg1, &reg2, &reg3, &reg4) << 4);
+		char mask6 = (Key(&reg1, &reg2, &reg3, &reg4) << 5);
+		char mask7 = (Key(&reg1, &reg2, &reg3, &reg4) << 6);
+		char mask8 = (Key(&reg1, &reg2, &reg3, &reg4) << 7);
 		unshifr[j] = ((shifr[j] ^ mask1) & 0x80) +
 			((shifr[j] ^ mask2) & 0x40) +
 			((shifr[j] ^ mask3) & 0x20) +
@@ -73,44 +139,32 @@ int main(int argc, char* argv[])
 			((shifr[j] ^ mask7) & 0x2) +
 			((shifr[j] ^ mask8) & 0x1);
 		ms[j] = msg_secret[j] ^ unshifr[j];
-
 		cout << ms[j];
-		// std::cout << std::bitset<sizeof(ms[j])* CHAR_BIT>(ms[j]) << "\n";
 	}
-	delete[] msg;
-	delete[] msg_secret;
-	delete[] shifr;
-	delete[] unshifr;
-	delete[] ms;
-	return 0;
 }
-int GetKey(unsigned short* r1, unsigned int* r2, unsigned int* r3, unsigned char* r4)
+int Key(unsigned short* reg1, unsigned int* reg2, unsigned int* reg3, unsigned char* reg4)
 {
-	unsigned int key = LFSR_Fibonacci1(r1) ^ LFSR_Fibonacci2(r2) ^ LFSR_Fibonacci3(r3) ^ LFSR_Fibonacci4(r4) ^ 1;
+	unsigned int key = LFSR1(reg1) ^ LFSR2(reg2) ^ LFSR3(reg3) ^ LFSR4(reg4) ^ 1;
 	return key;
 }
-int LFSR_Fibonacci1(unsigned short* r1)
+int LFSR1(unsigned short* reg1)
 {
-
-	*r1 = ((((*r1 >> 11) ^ (*r1 >> 10) ^ (*r1 >> 9) ^ (*r1 >> 3) ^ *r1) & 0x1) << 11) | (*r1 >> 1);
-	return *r1 & 0x1;
+	*reg1 = ((((*reg1 >> 11) ^ (*reg1 >> 10) ^ (*reg1 >> 9) ^ (*reg1 >> 3) ^ *reg1) & 0x1) << 11) | (*reg1 >> 1);
+	return *reg1 & 0x1;
 }
-int LFSR_Fibonacci2(unsigned int* r2)
+int LFSR2(unsigned int* reg2)
 {
-
-	*r2 = ((((*r2 >> 18) ^ (*r2 >> 17) ^ (*r2 >> 16) ^ (*r2 >> 13) ^ *r2) & 0x1) << 18) | (*r2 >> 1);
-	return *r2 & 0x1;
+	*reg2 = ((((*reg2 >> 18) ^ (*reg2 >> 17) ^ (*reg2 >> 16) ^ (*reg2 >> 13) ^ *reg2) & 0x1) << 18) | (*reg2 >> 1);
+	return *reg2 & 0x1;
 }
-int LFSR_Fibonacci3(unsigned int* r3)
+int LFSR3(unsigned int* reg3)
 {
-
-	*r3 = ((((*r3 >> 16) ^ (*r3 >> 13) ^ *r3) & 0x1) << 16) | (*r3 >> 1);
-	return *r3 & 0x1;
+	*reg3 = ((((*reg3 >> 16) ^ (*reg3 >> 13) ^ *reg3) & 0x1) << 16) | (*reg3 >> 1);
+	return *reg3 & 0x1;
 }
-int LFSR_Fibonacci4(unsigned char* r4)
+int LFSR4(unsigned char* reg4)
 {
-
-	*r4 = ((((*r4 >> 7) ^ (*r4 >> 5) ^ (*r4 >> 4) ^ *r4) & 0x1) << 7) | (*r4 >> 1);
-	return *r4 & 0x1;
+	*reg4 = ((((*reg4 >> 7) ^ (*reg4 >> 5) ^ (*reg4 >> 4) ^ *reg4) & 0x1) << 7) | (*reg4 >> 1);
+	return *reg4 & 0x1;
 }
 
